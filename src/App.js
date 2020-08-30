0e63baa857faca6cd6abcb99ec4408863729749a
sururuu <rlatnwls777@gmail.com>
2020-08-31 01:36:16 +0900

    navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import axios from "axios";
+import { HashRouter, Route } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
+import Navigation from "./components/Navigations";
 
 function App() {
-  return <span>somthing</span>
+  return <HashRouter>
+    <Navigation />
+    <Route path="/" exact={true} component={Home} />
+    <Route path="/about" component={About} />
+  </HashRouter>
 }
 
 export default App;
