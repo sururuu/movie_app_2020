a47e1cfb139a0002561b3d27ebd73313f18bbfa8
sujin <tkdgus700@naver.com>
2020-08-31 00:54:41 +0900

    Updates

---
+++ precache-manifest.5182a7a83de12cb544569846ac3e739b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bfb046813fe8c2a32a1f3045df3ecba",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "66123a6d7f5379b5822f",
+    "url": "/movie_app_2020/static/css/main.ae4cfe19.chunk.css"
+  },
+  {
+    "revision": "11748d6dc8da0fe94e79",
+    "url": "/movie_app_2020/static/js/2.8f3b569d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.8f3b569d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66123a6d7f5379b5822f",
+    "url": "/movie_app_2020/static/js/main.672aeeca.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
