e08bfda5a11042ad0bfae82f03a3850bd0f95c4e
sujin <rlatnwls777@gmail.com>
2020-09-10 18:35:07 +0900

    Sharing Props Between Routes, Redirecting

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import axios from "axios";
 import { HashRouter, Route } from "react-router-dom";
-import About from "./routes/About";
 import Home from "./routes/Home";
+import About from "./routes/About";
+import Detail from "./routes/detail";
 import Navigation from "./components/Navigations";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <Navigation />
     <Route path="/" exact={true} component={Home} />
     <Route path="/about" component={About} />
+    <Route path="/movie/:id" component={Detail} />
   </HashRouter>
 }
 
